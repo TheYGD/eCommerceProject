d956c7f2c498098204887c224337b30f33fb5fbb
TheYGD <jakub.szmidla@o2.pl>
2022-08-19 16:01:23 +0200

    changed htmls a little bit, now script can be passed to the fragment, added cart\show.html and script.js file where all the usefull, common scripts will be placed

---
+++ ecommerce-web/src/main/resources/static/js/script.js
@@ -0,0 +1,9 @@
+
+async function postData(url, data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data)
+    });
+
+    return response.json();
+}
