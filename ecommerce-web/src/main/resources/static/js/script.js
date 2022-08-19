0aa4f622481b82ecd995e735d4a42c0a4e3c57e6
TheYGD <jakub.szmidla@o2.pl>
2022-08-19 20:49:37 +0200

    changed script.js to make the code more universal, changed cart/show page

--- ecommerce-web/src/main/resources/static/js/script.js
+++ ecommerce-web/src/main/resources/static/js/script.js
@@ -1,7 +1,15 @@
 
+async function deleteRequest(url, data = {}) {
+    return makeRequest('DELETE', url, data);
+}
+
 async function postData(url, data = {}) {
+    return makeRequest('POST', url, data);
+}
+
+async function makeRequest(method, url, data) {
     const response = await fetch(url, {
-        method: 'POST',
+        method: method,
         body: JSON.stringify(data)
     });
 
