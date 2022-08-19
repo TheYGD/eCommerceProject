50769b9b4d0c9ba3d4c5dedc9c123ab8bd25e0f3
TheYGD <jakub.szmidla@o2.pl>
2022-08-19 21:01:29 +0200

    changed loginController, now when new user is register, they obtain a cart

--- ecommerce-web/src/main/java/pl/ecommerce/web/service/LoginService.java
+++ ecommerce-web/src/main/java/pl/ecommerce/web/service/LoginService.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import pl.ecommerce.data.dto.UserRegisterDto;
+import pl.ecommerce.data.entity.Cart;
 import pl.ecommerce.data.entity.User;
 import pl.ecommerce.data.entity.UserCredentials;
 import pl.ecommerce.data.mapper.UserMapper;
+import pl.ecommerce.repository.CartRepository;
 import pl.ecommerce.repository.UserCredentialsRepository;
 import pl.ecommerce.repository.UserRepository;
+
+import javax.transaction.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -18,6 +22,7 @@ public class LoginService {
 
     private final UserCredentialsRepository userCredentialsRepository;
     private final UserRepository userRepository;
+    private final CartRepository cartRepository;
     private final PasswordEncoder passwordEncoder;
 
 
@@ -46,12 +51,17 @@ public class LoginService {
         return userCredentialsRepository.findByUsername(username).isPresent();
     }
 
-    private void saveUser(UserRegisterDto userRegisterDto) {
+    @Transactional
+    public void saveUser(UserRegisterDto userRegisterDto) {
         userRegisterDto.setPassword( passwordEncoder.encode(userRegisterDto.getPassword()) );
         User user = UserMapper.INSTANCE.userRegisterDtoToUser( userRegisterDto );
 
         UserCredentials userCredentials = user.getCredentials();
         userCredentials.setUserAccount(user);
+
+        Cart cart = cartRepository.save(new Cart());
+        cart.setOwner(user);
+        user.setCart(cart);
 
         userCredentialsRepository.save(userCredentials);
         userRepository.save(user);
