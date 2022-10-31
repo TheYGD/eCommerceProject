4fa0df0cd006f24953d9c416902eb9663346f17b
TheYGD <xurexpl@gmail.com>
2022-10-31 01:57:02 +0100

    Changed Bootstrap class to show more functionalities

--- ecommerce-web/src/main/java/pl/ecommerce/web/bootstrap/Bootstrap.java
+++ ecommerce-web/src/main/java/pl/ecommerce/web/bootstrap/Bootstrap.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import pl.ecommerce.admin.domain.Account;
@@ -16,6 +17,7 @@ import pl.ecommerce.repository.*;
 
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * This class bootstraps the application with some data to help with development
@@ -32,6 +34,9 @@ public class Bootstrap implements ApplicationListener<ContextRefreshedEvent> {
     private final UserCredentialsRepository userCredentialsRepository;
     private final CartRepository cartRepository;
     private final AccountRepository accountRepository;
+    private final PseudoEnumRepository pseudoEnumRepository;
+    private final CategoryAttributeRepository categoryAttributeRepository;
+    private final ProductAttributeRepository productAttributeRepository;
 
     private final PasswordEncoder passwordEncoder;
 
@@ -87,7 +92,18 @@ public class Bootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
         Category category1 = new Category("Samochody", "Znajdują się tutaj samochody");
         category1.setOrderId(2);
+        category1.setParent(rootCategory);
         categoryRepository.save(category1);
+
+        CategoryAttribute attr1 = new CategoryAttribute(category1, "Rok produkcji", true, null);
+        PseudoEnum pseudoEnum = new PseudoEnum();
+        PseudoEnumValue val1 = new PseudoEnumValue(1, pseudoEnum, "Osobowe");
+        PseudoEnumValue val2 = new PseudoEnumValue(2, pseudoEnum, "Ciężarowe");
+        pseudoEnum.setValues(List.of(val1, val2));
+        CategoryAttribute attr2 = new CategoryAttribute(category1, "Typ", false, pseudoEnum);
+
+        pseudoEnumRepository.save(pseudoEnum);
+        categoryAttributeRepository.saveAll(List.of(attr1, attr2));
 
 
         Product product1 = productRepository.save( new Product("Audi1", "szybkie 2 audi", category1, seller1,
@@ -95,24 +111,30 @@ public class Bootstrap implements ApplicationListener<ContextRefreshedEvent> {
         AvailableProduct availableProduct1 = new AvailableProduct(product1, 1, 0, false);
         availableProduct1.setId(product1.getId());
         product1.setAvailableProduct(availableProduct1);
+        ProductAttribute pAttr1 = new ProductAttribute(attr1, product1, BigDecimal.valueOf(2008));
+        ProductAttribute pAttr2 = new ProductAttribute(attr2, product1, BigDecimal.valueOf(1));
 
         Product product2 = productRepository.save( new Product("BMW1", "szybkie 2 bmw", category1, seller1,
                 BigDecimal.valueOf(120000), null) );
-        AvailableProduct availableProduct2 = new AvailableProduct(product2, 1, 0, false);
+        AvailableProduct availableProduct2 = new AvailableProduct(product2, 2, 0, false);
         availableProduct2.setId(product2.getId());
         product2.setAvailableProduct(availableProduct2);
+        ProductAttribute pAttr3 = new ProductAttribute(attr1, product2, BigDecimal.valueOf(2015));
+        ProductAttribute pAttr4 = new ProductAttribute(attr2, product2, BigDecimal.valueOf(1));
 
 
         Product product3 = productRepository.save( new Product("Mercedes1", "szybki 2 mercedes", category1, seller1,
                 BigDecimal.valueOf(150000), null) );
-        AvailableProduct availableProduct3 = new AvailableProduct(product3, 1, 0, false);
+        AvailableProduct availableProduct3 = new AvailableProduct(product3, 3, 0, false);
         availableProduct3.setId(product3.getId());
         product3.setAvailableProduct(availableProduct3);
-
+        ProductAttribute pAttr5 = new ProductAttribute(attr1, product3, BigDecimal.valueOf(2012));
+        ProductAttribute pAttr6 = new ProductAttribute(attr2, product3, BigDecimal.valueOf(1));
 
         productRepository.save(product1);
         productRepository.save(product2);
         productRepository.save(product3);
+        productAttributeRepository.saveAll(List.of(pAttr1, pAttr2, pAttr3, pAttr4, pAttr5, pAttr6));
     }
 
 
